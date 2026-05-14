bb44ef434824cd00be8d846d8dc8d4ca8ea6def6
MatthewCaruana <matthew.caruana99@gmail.com>
2026-05-15 00:31:33 +0200

    Attempt at front end changes

--- MatthewSandPit.ClientSide/src/main.ts
+++ MatthewSandPit.ClientSide/src/main.ts
@@ -1,6 +1,8 @@
 import './assets/main.css'
+import './assets/custom.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
